d355199f7379a53a747218ac27890340b8e461d3
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 12:39:12 +0700

    tag view

---
+++ TagView/TagView/TagViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagViewApp.swift
+//  TagView
+//
+//  Created by Adrian Suryo Abiyoga on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TagViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
